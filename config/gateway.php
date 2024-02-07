746372c0cec9fb093cd982164be05a8cbf017e68
alihamzehei <alihamzehei2017@gmail.com>
2024-02-08 00:12:01 +0330

    refactored

--- config/gateway.php
+++ config/gateway.php
@@ -3,18 +3,13 @@
 return [
     'services' => [
         'authsvc' => [
-            'url' => 'https://jsonplaceholder.typicode.com',
-            'port' => 443,
-            'timeout' => 10,
-        ],
-        'usersvc' => [
             'url' => 'http://127.0.0.1',
             'port' => 8001,
             'timeout' => 10,
         ],
         'todosvc' => [
             'url' => 'http://127.0.0.1',
-            'port' => 8000,
+            'port' => 8002,
             'timeout' => 10,
         ],
     ],
